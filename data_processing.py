ddf069278d4730c34f0ba361da7f8604fca60e2f
Thanakorn JAEMPRASERTKUL <thanakorn.jae@ku.th>
2023-12-07 08:45:17 +0700

    solution quiz

--- data_processing.py
+++ data_processing.py
@@ -1,19 +1,14 @@
 import csv, os
 
-# __location__ = os.path.realpath(
-#     os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-class Readcsv:
-    def __init__(self, filename):
-        self.data = []
-        self.filename = filename
-        self.__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+movies = []
+with open(os.path.join(__location__, 'movies.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        movies.append(dict(r))
 
-    def read_csv(self):
-        with open(self.filename) as file:
-            csv_reader = csv.DictReader(file)
-            for row in csv_reader:
-                self.data.append(dict(row))
 
 class DB:
     def __init__(self):
@@ -27,13 +22,24 @@ class DB:
             if table.table_name == table_name:
                 return table
         return None
-    
+
+
 import copy
+
+
 class Table:
     def __init__(self, table_name, table):
         self.table_name = table_name
         self.table = table
-    
+
+    def insert_row(self, dict):
+        self.table.append(dict)
+
+    def update_row(self, primary_attribute, primary_attribute_value, update_attribute, update_value):
+        for i in self.table:
+            if i[primary_attribute] == primary_attribute_value:
+                i[update_attribute] = update_value
+
     def join(self, other_table, common_key):
         joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
         for item1 in self.table:
@@ -44,7 +50,7 @@ class Table:
                     dict1.update(dict2)
                     joined_table.table.append(dict1)
         return joined_table
-    
+
     def filter(self, condition):
         filtered_table = Table(self.table_name + '_filtered', [])
         for item1 in self.table:
@@ -52,27 +58,8 @@ class Table:
                 filtered_table.table.append(item1)
         return filtered_table
 
-    def insert_row(self, diction):
-        '''
-        This method inserts a dictionary, dict, into a Table object, effectively adding a row to the Table.
-        '''
-        if isinstance(diction, dict):
-            self.table.append(dict)
-
-
-    def update_row(self, primary_attribute, primary_attribute_value, update_attribute, update_value):
-        '''
-        This method updates the current value of update_attribute to update_value
-        For example, my_table.update_row('Film', 'A Serious Man', 'Year', '2022') will change the 'Year' attribute for the 'Film'
-        'A Serious Man' from 2009 to 2022
-        '''
-        for i in self.table:
-            primary_attribute = list(i.keys())[-1]
-            if i[primary_attribute] == update_attribute:
-                i[primary_attribute_value] = update_value
-
     def __is_float(self, element):
-        if element is None: 
+        if element is None:
             return False
         try:
             float(element)
@@ -88,7 +75,7 @@ class Table:
             else:
                 temps.append(item1[aggregation_key])
         return function(temps)
-    
+
     def select(self, attributes_list):
         temps = []
         for item1 in self.table:
@@ -131,3 +118,32 @@ class Table:
     def __str__(self):
         return self.table_name + ':' + str(self.table)
 
+
+movies_table = Table("Movies", movies)
+my_DB = DB()
+my_DB.insert(movies_table)
+
+my_movies_table = my_DB.search("Movies")
+my_movies_table_filter_comedy = movies_table.filter(lambda x: x["Genre"] == "Comedy")
+my_movies_table_filter_fantasy = movies_table.filter(lambda x: x["Genre"] == "Fantasy")
+print(len(my_movies_table_filter_fantasy.table))
+dict = {}
+dict['Film'] = 'The Shape of Water'
+dict['Genre'] = 'Fantasy'
+dict['Lead Studio'] = 'Fox'
+dict['Audience score %'] = '72'
+dict['Profitability'] = '9.765'
+dict['Rotten Tomatoes %'] = '92'
+dict['Worldwide Gross'] = '195.3'
+dict['Year'] = '2017'
+
+movies_table.insert_row(dict)
+print(movies_table)
+# movies_table.update_row('Film', 'A Serious Man', 'Year', '2022')
+# movies_table.insert_row()
+# my_movies_table_filter_fantasy = movies_table.filter(lambda x: x["Genre"] == "Fantasy")
+# print(my_movies_table_filter_fantasy.aggregate(lambda x: len(x)))
+print(my_movies_table_filter_comedy.aggregate(lambda x: sum(x) / len(x), "Worldwide Gross"))
+
+my_movies_table_filter_drama = movies_table.filter(lambda x: x["Genre"] == "Drama")
+print(my_movies_table_filter_drama.aggregate(lambda x: min(x), "Audience score %"))
